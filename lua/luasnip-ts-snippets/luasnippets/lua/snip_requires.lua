e9215c60fe6c51089e65819ded2f821fa2590e14
Filip Godlewski <54432731+filipgodlewski@users.noreply.github.com>
2022-11-24 19:42:45 +0100

    fix: 🐛 replace '-' with '_' for local names in lua

--- lua/luasnip-ts-snippets/luasnippets/lua/snip_requires.lua
+++ lua/luasnip-ts-snippets/luasnippets/lua/snip_requires.lua
@@ -1,6 +1,13 @@
 local l = require("luasnip.session").config.snip_env
 local u = require "luasnip-ts-snippets.utils.snip"
 
+local function last(index, sep)
+   return l.f(function(name)
+      local parts = vim.split(name[1][1], sep, true)
+      return string.gsub(parts[#parts], "-", "_") or ""
+   end, { index })
+end
+
 return {
    l.s(
       {
@@ -10,7 +17,7 @@ return {
       },
       l.c(1, {
          l.sn(nil, l.fmta('require("<>").<>', { l.i(1), l.i(2, "setup") }), u.desc "Normal"),
-         l.sn(nil, l.fmta('local <> = require "<>"<>', { u.last_text(1, "."), l.i(1), l.i(2) }), u.desc "Import"),
+         l.sn(nil, l.fmta('local <> = require "<>"<>', { last(1, "."), l.i(1), l.i(2) }), u.desc "Import"),
       })
    ),
 }
--- lua/luasnip-ts-snippets/utils/snip/init.lua
+++ lua/luasnip-ts-snippets/utils/snip/init.lua
@@ -1,19 +1,7 @@
 local cfg = require("luasnip-ts-snippets").cfg
 local ls = require "luasnip"
-local f = ls.function_node
 
 local M = {}
-
--- get last item after 'sep' in require function
--- @param index integer of the node referred to
--- @param sep string the split separator
--- @return string the last split item
-function M.last_text(index, sep)
-   return f(function(name)
-      local parts = vim.split(name[1][1], sep, true)
-      return parts[#parts] or ""
-   end, { index })
-end
 
 -- set virt text description for nodes
 -- @param text string wha you want to print to virt text
